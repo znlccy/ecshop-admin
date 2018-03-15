186cf8ded1f46de74be3060be37dcc604f1a90b9
znlccy <756311868@qq.com>
2018-03-15 13:04:06 +0800

    添加邮件发送工具

--- src/main/java/com/znlccy/ecshop/controller/EmailController.java
+++ src/main/java/com/znlccy/ecshop/controller/EmailController.java
@@ -1,6 +1,13 @@
 package com.znlccy.ecshop.controller;
 
+import com.znlccy.ecshop.config.EmailConfig;
+import com.znlccy.ecshop.service.UserService;
+import com.znlccy.ecshop.tools.RandomTools;
+import com.znlccy.ecshop.utils.SendEmailUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @CreateTime:2018/3/15 11:42
@@ -13,10 +20,41 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(maxAge = 3600, origins = "*")
 public class EmailController {
 
+    /**
+     * 自动依赖注入userService
+     */
+    /*@Autowired
+    private UserService userService;*/
+
+    /**
+     * 自动依赖注入sendEmailUtils
+     */
+    @Autowired
+    private SendEmailUtils sendEmailUtils;
+
+    /**
+     * 自动依赖注入emailConfig
+     */
+    @Autowired
+    private EmailConfig emailConfig;
+
+    /**
+     * @comment: sendCode 发送验证码
+     * @param: [email, request]
+     * @return: void
+     */
     @RequestMapping(value = "/send", method = RequestMethod.POST)
     @ResponseBody
-    public void sendEmail() {
-
+    public String sendCode(@RequestParam(value = "email",defaultValue = "", required = true) String email, HttpServletRequest request) {
+        try {
+            String code = RandomTools.randomCode();
+            request.getSession().setAttribute("registerCode", code);
+            sendEmailUtils.sendRegisterCode(email, code);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "0";
+        }
+        return "1";
     }
 
 }
--- src/main/java/com/znlccy/ecshop/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/znlccy/ecshop/serviceImpl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.znlccy.ecshop.dao.UserDao;
 import com.znlccy.ecshop.model.User;
 import com.znlccy.ecshop.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ import java.util.List;
  * @Version:v-1.0.0
  * @Comment: 用户服务接口实现类
  */
+@Service
 public class UserServiceImpl implements UserService {
 
     /**
